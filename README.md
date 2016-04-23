fff685cbc774b950166743ffb8d4bb5a9a1736e1
stevleibelt <stev@leibelt.de>
2016-04-23 22:11:56 +0200

    added update capability

--- README.md
+++ README.md
@@ -1,15 +1,32 @@
 # stev leibelt config for shell
 
-A bootstrapfile that loads all files and if exists local files.
+This repostiry devides shell configuration into logical parts to gain overview and configuration flexibility.
+
+The code comes with a predefined set of configuration settings.
+
+The logical parts are:
+* setting
+* variable
+* source
+* export
+* function
+* alias
+* automatic_start
+
+You can overwrite or extend a logical part by creating a "local.<logical part file name>" file. This file is loaded after the original file.
 
 # Installation
 
 * git clone the repository int /your/path.
 * execute install.sh
 
+# Updating
+
+* execute update.sh
+
 # To Do
 
-* create update.sh
+* extend update.sh
     * removes existing adaption of the bashrc
     * recalls install.sh
 * create clean
---
+++ update.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+########
+# Migration script for bigger changes
+#
+# @author stev leibelt
+# @since 2016-04-23
+########
+
+LOCAL_CURRENT_WORKING_DIRECTORY=$(pwd)
+LOCAL_PATH_TO_THIS_SCRIPT=$(cd $(dirname "$0"); pwd)
+LOCAL_PATH_TO_CURRENT_VERSION_FILE=$LOCAL_PATH_TO_THIS_SCRIPT"/.current_version"
+LOCAL_PATH_TO_CURRENT_INSTALLED_VERSION_FILE=$LOCAL_PATH_TO_THIS_SCRIPT"/.current_installed_version"
+
+cd $LOCAL_PATH_TO_THIS_SCRIPT
+
+echo "updating repository."
+git pull --quiet
+
+if [[ ! -f $LOCAL_PATH_TO_CURRENT_INSTALLED_VERSION_FILE ]]
+then
+    #version 0
+    echo 0 > $LOCAL_PATH_TO_CURRENT_INSTALLED_VERSION_FILE
+fi
+
+LOCAL_CURRENT_VERSION=$(cat $LOCAL_PATH_TO_CURRENT_VERSION_FILE)
+LOCAL_CURRENT_INSTALLED_VERSION=$(cat $LOCAL_PATH_TO_CURRENT_INSTALLED_VERSION_FILE)
+
+if [[ $LOCAL_CURRENT_VERSION -eq $LOCAL_CURRENT_INSTALLED_VERSION ]]
+then
+    echo "latest version already installed. nothing to do."
+else
+    #begin of version 0 migration
+    if [[ $LOCAL_CURRENT_INSTALLED_VERSION -eq 0 ]]
+    then
+        echo "migrating from version $LOCAL_CURRENT_INSTALLED_VERSION to "$(($LOCAL_CURRENT_INSTALLED_VERSION + 1))
+
+        declare -a LOCAL_FILES_TO_RENAME=('setting' 'variable' 'source' 'export' 'function' 'alias' 'automatic_start')
+
+        for LOCAL_FILE_TO_RENAME in ${LOCAL_FILES_TO_RENAME[@]}; do
+            LOCAL_DESTINATION_FILE_PATH=$LOCAL_PATH_TO_THIS_SCRIPT"/local."$LOCAL_FILE_TO_RENAME
+            LOCAL_SOURCE_FILE_PATH=$LOCAL_PATH_TO_THIS_SCRIPT"/"$LOCAL_FILE_TO_RENAME".local"
+
+            #does local source file exist?
+            if [[ -f $LOCAL_SOURCE_FILE_PATH ]]
+            then
+                #does local destionation file exist?
+                if [[ -f $LOCAL_DESTINATION_FILE_PATH ]]
+                then
+                    echo "file $LOCAL_DESTINATION_FILE_PATH already exist"
+                    echo "can not move $LOCAL_SOURCE_FILE_PATH"
+                    echo "please merge it manually"
+                    echo ""
+                else
+                    mv $LOCAL_SOURCE_FILE_PATH $LOCAL_DESTINATION_FILE_PATH
+                fi
+            fi
+        done;
+
+        LOCAL_CURRENT_INSTALLED_VERSION=$((LOCAL_CURRENT_VERSION + 1))
+    fi
+    #end of version 0 migration
+
+    #begin of updating currently installed version
+    echo $LOCAL_CURRENT_VERSION > $LOCAL_PATH_TO_CURRENT_INSTALLED_VERSION_FILE
+    #end of updating currently installed version
+fi
+
+cd $LOCAL_CURRENT_WORKING_DIRECTORY
