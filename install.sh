13e4b4c906d8fd1f795731cf6f14df3f97f2e8e8
stevleibelt <stev@leibelt.de>
2017-03-09 22:46:20 +0100

    styled

--- install.sh
+++ install.sh
@@ -10,8 +10,17 @@
 
 clear
 
-PATH_TO_THE_BASH_RC="${HOME}/.bashrc"
+#begin of setup
+DATETIME=$(date +%y%m%d_+%T)
 
+PATH_TO_THE_BASH_RC="${HOME}/.bashrc"
+PATH_TO_THIS_SCRIPT=$(cd $(dirname "$0"); pwd)
+PATH_TO_THE_XINIT="${HOME}/.xinitrc"
+
+PATH_TO_THE_TEMPORARY_XINIT="${PATH_TO_THE_XINIT}.temporary"
+#end of setup
+
+##begin of validation
 if [[ ! -f "${PATH_TO_THE_BASH_RC}" ]];
 then
     echo "No .bashrc file found."
@@ -19,64 +28,78 @@ then
 
     exit 1
 fi
+##end of validation
 
-PATH_TO_THIS_SCRIPT=$(cd $(dirname "$0"); pwd)
-DATETIME=$(date +%y%m%d_+%T)
-
+##begin of adapting bashrc
 echo "Adapting .bashrc"
 
-echo "#net_bazzline_config_shell start" >> ${PATH_TO_THE_BASH_RC}
-echo "#date: "${DATETIME} >> ${PATH_TO_THE_BASH_RC}
-echo "" >> ${PATH_TO_THE_BASH_RC}
-echo "PATH_SHELL_CONFIG=\${PATH_TO_THIS_SCRIPT}" >> ${PATH_TO_THE_BASH_RC}
-echo "if [ -f \${PATH_SHELL_CONFIG}\"/bootstrap\" ]; then " >> ${PATH_TO_THE_BASH_RC}
-echo "  source \${PATH_SHELL_CONFIG}\"/bootstrap\"" >> ${PATH_TO_THE_BASH_RC}
-echo "fi" >> ${PATH_TO_THE_BASH_RC}
-echo "" >> ${PATH_TO_THE_BASH_RC}
-echo "#net_bazzline_config_shell end" >> ${PATH_TO_THE_BASH_RC}
+cat >> "${PATH_TO_THE_BASH_RC}" <<DELIM
+#begin of net_bazzline_config_shell"
+#date: "${DATETIME}
 
+PATH_SHELL_CONFIG="${PATH_TO_THIS_SCRIPT}"
+
+if [[ -f "\${PATH_SHELL_CONFIG}/bootstrap" ]];
+then
+    source "\${PATH_SHELL_CONFIG}/bootstrap"
+fi
+#end of net_bazzline_config_shell"
+DELIM
+##end of adapting bashrc
+
+##begin of creating local user files
 echo ""
 echo "Creating local files"
+
 declare -a FILES_TO_CREATE=("setting" "variable" "source" "export" "function" "alias" "automatic_start")
+
 for FILE_TO_CREATE in ${FILES_TO_CREATE[@]}; do
     FILE_PATH_NAME="${PATH_TO_THIS_SCRIPT}/local.${FILE_TO_CREATE}"
     touch "${FILE_PATH_NAME}"
-    echo "#!/bin/bash" > "${PATH_TO_THE_BASH_RC}"
+    echo "#!/bin/bash" > "${FILE_PATH_NAME}"
 done;
+##end of creating local user files
 
-PATH_TO_THE_XINIT="${HOME}/.xinitrc.temp"
-PATH_TO_THE_TEMPORARY_XINIT="${PATH_TO_THE_XINIT}.temporary"
-
+##begin of adating xínitrc
 echo ""
 echo "Creating .xinitrc.temporary"
+
 touch "${PATH_TO_THE_TEMPORARY_XINIT}"
 
-echo "Adding content to .xinitrc.temporary"
+echo "Adding content to "basename(${PATH_TO_THE_TEMPORARY_XINIT})
 
-echo "#net_bazzline_config_shell start" >> "${PATH_TO_THE_TEMPORARY_XINIT}"
-echo "#date: "${DATETIME} >> ${PATH_TO_THE_TEMPORARY_XINIT}
-echo "" >> ${PATH_TO_THE_TEMPORARY_XINIT}
-echo "PATH_SHELL_CONFIG=\${PATH_TO_THIS_SCRIPT}" >> ${PATH_TO_THE_TEMPORARY_XINIT}
-echo "if [[ -f \${PATH_SHELL_CONFIG}\"/color\" ]]; then " >> ${PATH_TO_THE_TEMPORARY_XINIT}
-echo "  xrdb -merge \${PATH_SHELL_CONFIG}\"/color\"" >> ${PATH_TO_THE_TEMPORARY_XINIT}
-echo "fi" >> ${PATH_TO_THE_TEMPORARY_XINIT}
-echo "if [[ -f \${PATH_SHELL_CONFIG}\"/local.color\" ]]; then " >> ${PATH_TO_THE_TEMPORARY_XINIT}
-echo "  xrdb -merge \${PATH_SHELL_CONFIG}\"/local.color\"" >> ${PATH_TO_THE_TEMPORARY_XINIT}
-echo "fi" >> ${PATH_TO_THE_TEMPORARY_XINIT}
-echo "" >> ${PATH_TO_THE_TEMPORARY_XINIT}
-echo "#net_bazzline_config_shell end" >> ${PATH_TO_THE_TEMPORARY_XINIT}
-echo "" >> ${PATH_TO_THE_TEMPORARY_XINIT}
+cat >> "${PATH_TO_THE_TEMPORARY_XINIT}" <<DELIM
+#begin of net_bazzline_config_shell"
+#date: "${DATETIME}
+
+PATH_SHELL_CONFIG="\${PATH_TO_THIS_SCRIPT}"
+
+if [[ -f "${PATH_SHELL_CONFIG}/color" ]];
+then
+  xrdb -merge "${PATH_SHELL_CONFIG}/color"
+fi
+if [[ -f "${PATH_SHELL_CONFIG}/color.local" ]];
+then
+  xrdb -merge "${PATH_SHELL_CONFIG}/color.local"
+fi
+
+#end of net_bazzline_config_shell"
+
+DELIM
 
 echo ""
 echo "Copying content of .xinitrc to .xinitrc.temporary"
 
-cat ${PATH_TO_THE_XINIT} >> ${PATH_TO_THE_TEMPORARY_XINIT}
+cat "${PATH_TO_THE_XINIT}" >> "${PATH_TO_THE_TEMPORARY_XINIT}"
 
 echo ""
 echo "Replacing .xinitrc with .xinitrc.temporary"
 
-mv ${PATH_TO_THE_TEMPORARY_XINIT} ${PATH_TO_THE_XINIT}
-rm ${PATH_TO_THE_TEMPORARY_XINIT}
+mv "${PATH_TO_THE_TEMPORARY_XINIT}" "${PATH_TO_THE_XINIT}"
+rm "${PATH_TO_THE_TEMPORARY_XINIT}"
+##begin of adating xínitrc
 
+##begin of finish
 echo ""
 echo "Finished"
+##begin of finish
