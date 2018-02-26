0e881335441e6d96dc3d1af903f81bdab9181877
stevleibelt <stev@leibelt.de>
2018-02-26 08:40:34 +0100

    added idea

--- manage_local_settings_wip.sh
+++ manage_local_settings_wip.sh
@@ -12,7 +12,15 @@ PATH_TO_THIS_SCRIPT=$(cd $(dirname "$0"); pwd)
 cd ${PATH_TO_THIS_SCRIPT}
 
 #backup existing local.setting file
+#fetch all available option by cat setting
 #ask for package manager backend (apt, pacman, pacaur, yaourt)
+
+## begin of fetch available option
+echo ":: Listing available options."
+cat ${PATH_TO_THIS_SCRIPT}/setting | grep NET_BAZZLINE_
+## end of fetch available option
+
+#sed -i -e 's/abc/XYZ/g' /tmp/file.txt
 
 echo ":: Reloading bash environment."
 source ~/.bashrc && clear
