34d652719ac1d53812edbae26d2867ceb754e9ee
stevleibelt <stev@leibelt.de>
2019-05-30 22:58:47 +0200

    updated

---
+++ installation/general/no_bee�p.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+####
+# @see
+#   https://wiki.archlinux.org/index.php/PC_speaker#Disable_PC_Speaker
+####
+# @since 2019-05-31
+# @author stev leibelt <artodeto@bazzline.net>
+####
+
+if [[  -f /etc/modprobe.d/nobeeb.conf ]];
+then
+    echo ":: File >>/etc/modeprobe.conf/nobeeb.conf<< exists already!"
+else
+    sudo bash -c "cat > /etc/modprobe.d/nobeeb.conf <<DELIM
+#@see: https://wiki.archlinux.org/index.php/PC_speaker#Disable_PC_Speaker
+blacklist pcspkr
+DELIM"
+
+    echo ":: File >>/etc/modeprobe.conf/nobeeb.conf<< created."
+fi
