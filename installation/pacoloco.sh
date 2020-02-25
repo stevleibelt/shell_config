a31ff91845f2d5ddd21aaa497862fc6c1ca17d2c
stevleibelt <stev@leibelt.de>
2020-02-25 20:44:44 +0100

    moved into function to not screw up gobal variable space

--- installation/pacoloco.sh
+++ installation/pacoloco.sh
@@ -6,37 +6,38 @@
 # @author stev leibelt <artodeto@arcor.de>
 ####
 
-if [[ ! -f /usr/bin/yay ]];
-then
-    echo ":: Aborting."
-    echo "   No /usr/bin/yay installed."
+function install_pacoloco_if_needed() {
+    if [[ ! -f /usr/bin/yay ]];
+    then
+        echo ":: Aborting."
+        echo "   No /usr/bin/yay installed."
 
-    exit 1
-fi
+        exit 1
+    fi
 
-if [[ ! -f /usr/bin/pacoloco ]];
-then
-    echo ":: Installing pacoloco."
+    if [[ ! -f /usr/bin/pacoloco ]];
+    then
+        echo ":: Installing pacoloco."
 
-    yay -S pacoloco-git
-fi
+        yay -S pacoloco-git
+    fi
 
-if [[ -f /etc/pacoloco.yaml ]];
-then
-    local CURRENT_DATETIME=$(date +'%Y%m%d.%H%M%S')
+    if [[ -f /etc/pacoloco.yaml ]];
+    then
+        local CURRENT_DATETIME=$(date +'%Y%m%d.%H%M')
 
-    echo ":: Moving existing file to /etc/pacoloco.yaml.${CURRENT_DATETIME}."
-    sudo mv /etc/pacoloco.yaml "/etc/pacoloco.yaml.${CURRENT_DATETIME}"
-fi
+        echo ":: Moving existing file to /etc/pacoloco.yaml.${CURRENT_DATETIME}."
+        sudo mv /etc/pacoloco.yaml "/etc/pacoloco.yaml.${CURRENT_DATETIME}"
+    fi
 
-if [[ ! -d /var/cache/pacoloco ]];
-then
-    echo ":: Creating cache path /var/cache/pacoloco."
+    if [[ ! -d /var/cache/pacoloco ]];
+    then
+        echo ":: Creating cache path /var/cache/pacoloco."
 
-    sudo /usr/bin/mkdir -p /var/cache/pacoloco
-fi
+        sudo /usr/bin/mkdir -p /var/cache/pacoloco
+    fi
 
-sudo bash -c "cat >/etc/pacoloco.yaml<<DELIM
+    sudo bash -c "cat >/etc/pacoloco.yaml<<DELIM
 ---
 echo cache_dir: /var/cache/pacoloco
 port: 9129
@@ -62,8 +63,11 @@ repos:
       - http://mirror.archlinuxarm.org/
 DELIM"
 
-echo ":: Do not forget to add the url of this server to the pacman mirror list."
-echo "   1.) for arm6vh"
-echo "   http://myserver:9129/repo/archlinux_\$arch/\$arch/\$repo"
-echo "   2.) for x86_64"
-echo "   http://myserver:9129/repo/archlinux_\$arch/\$repo/os\$arch"
+    echo ":: Do not forget to add the url of this server to the pacman mirror list."
+    echo "   1.) for arm6vh"
+    echo "   http://myserver:9129/repo/archlinux_\$arch/\$arch/\$repo"
+    echo "   2.) for x86_64"
+    echo "   http://myserver:9129/repo/archlinux_\$arch/\$repo/os\$arch"
+}
+
+install_pacoloco_if_needed
