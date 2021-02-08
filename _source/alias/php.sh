0607233e326f218e8c413652b1168a917d777789
stevleibelt <stev@leibelt.de>
2021-02-08 15:02:16 +0100

    added alias lintPhpCode

--- _source/alias/php.sh
+++ _source/alias/php.sh
@@ -8,5 +8,9 @@
 
 if [[ ${NET_BAZZLINE_PHP_IS_AVAILABLE} -eq 1 ]];
 then
+    #l
+    alias lintPhpCode="find . -name *.php | xargs -n1 php -l | grep -v 'No syntax errors detected'"
+
+    #s
     alias startPhpWebserver=net_bazzline_php_start_internal_webserver
 fi
