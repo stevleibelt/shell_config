912bc30d13e514958c552f6375c2e59381a9bb28
stevleibelt <stev@leibelt.de>
2021-12-11 22:50:04 +0100

    added paru

---
+++ setup/package_manager/paru.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+####
+# @see: https://aur.archlinux.org/paru.git
+# @since 2021-12-11
+# @author stev leibelt <artodeto@bazzline.net>
+####
+
+#begin of testing if we are on the right system
+if [[ ! -f /usr/bin/pacman ]];
+then
+    echo ":: Can not install on your system."
+    echo "   Sorry dude, I can only install things on a arch linux."
+
+    exit 1
+fi
+
+sudo pacman -Syy
+#end of testing if we are on the right system
+
+if [[ ! -f /usr/bin/git ]];
+then
+    echo ":: Please install git."
+    
+    exit 2
+fi
+
+if [[ ! -f /usr/bin/pactree ]];
+then
+    echo ":: Pactree is missing but mandatory."
+    echo ":: Installing pactre."
+
+    sudo pacman -S pacman-contrib
+fi
+
+CURRENT_WORKING_DIRECTORY=$(pwd)
+
+##begin of temporary path creation
+TEMPORARY_DIRECTORY_PATH=$(mktemp -d)
+cd ${TEMPORARY_DIRECTORY_PATH}
+##begin of temporary path creation
+
+##begin of building and installing
+cd ${TEMPORARY_DIRECTORY_PATH}
+#git clone https://aur.archlinux.org/yay.git .
+git clone https://aur.archlinux.org/paru.git .
+makepkg -si
+##end of building and installing
+
+##begin of clean up
+cd ${CURRENT_WORKING_DIRECTORY}
+rm -fr {TEMPORARY_DIRECTORY_PATH}
+##end of clean up
