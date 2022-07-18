8769e3a793ba18a0ddc02132a4c99cecb0ed2ba0
Stev Leibelt <stev.leibelt@hrz.tu-freiberg.de>
2022-07-18 06:55:12 +0200

    added missing source list

--- _source/alias/cpu.sh
+++ _source/alias/cpu.sh
@@ -3,5 +3,6 @@
 #l
 if [[ ${NET_BAZZLINE_OPERATION_SYSTEM_IS_LINUX} -eq 1 ]];
 then
+    #@see: https://www.phoronix.com/scan.php?page=article&item=retbleed-benchmark&num=1
     alias listCpuVulnerabilities="ls /sys/devices/system/cpu/vulnerabilities/*"
 fi
