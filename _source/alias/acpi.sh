d11bc2cca405d28887f4fed1003b8423cebfa593
stevleibelt <artodeto@bazzline.net>
2022-12-29 17:30:47 +0100

    fixed an issue

--- _source/alias/acpi.sh
+++ _source/alias/acpi.sh
@@ -8,6 +8,7 @@
 
 if [[ ${NET_BAZZLINE_ACPI_IS_AVAILABLE} -eq 1 ]];
 then
-    alias showBatteryStatusWithoutAcpi='for BATTERY_FILE_PATH in $(find /sys/class/power_supply/ -iname "BAT*");do echo ${BATTERY_FILE_PATH}; cat ${BATTERY_FILE_PATH}/capacity; cat ${BATTERY_FILE_PATH}/status; done;'
     alias showBatteryStatusWithAcpi='acpi -i'
+else
+    alias showBatteryStatusWithoutAcpi='for BATTERY_FILE_PATH in $(find /sys/class/power_supply/ -iname "BAT*");do echo ${BATTERY_FILE_PATH}; cat ${BATTERY_FILE_PATH}/capacity; cat ${BATTERY_FILE_PATH}/status; done;'
 fi
