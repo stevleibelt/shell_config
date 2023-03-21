d05fe0aa02ade54fe5cc633f21a0ca8446dde888
stevleibelt <artodeto@bazzline.net>
2023-03-21 08:53:09 +0100

    added >>gnome-terminal<<

--- bin/setup/general/install_packages.sh
+++ bin/setup/general/install_packages.sh
@@ -70,7 +70,7 @@ function _install_stage_3 ()
   _install_packages_with_yay firefox ffmpeg flameshot freeplan fwupd
   
   echo "   Processing packages with >>g<<"
-  _install_packages_with_yay gcc gcc-libs geeqie geany gimp git gparted glibc gvfs gvfs-mtb gvfs-smb
+  _install_packages_with_yay gcc gcc-libs geeqie geany gimp git gnome-terminal gparted glibc gvfs gvfs-mtb gvfs-smb
   
   echo "   Processing packages with >>h<<"
   _install_packages_with_yay hdparm htop hwinfo
