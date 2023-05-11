13d8bdba21e68ae5cedcf47525d0ba1c0195be8c
stevleibelt <stev.leibelt@hrz.tu-freiberg.de>
2023-05-11 10:45:20 +0200

    Add jq section below string with alias and function of 'decodeBase64Jwt=net_bazzline_string_jq_decode_base64_jwt_token'

--- _source/alias/string.sh
+++ _source/alias/string.sh
@@ -9,3 +9,8 @@ alias leftTrim=net_bazzline_string_left_trim
 alias rightTrim=net_bazzline_string_right_trim
 alias trim=net_bazzline_string_trim
 
+if [[ -f /usr/bin/jq ]];
+then
+  alias decodeBase64Jwt=net_bazzline_string_jq_decode_base64_jwt_token
+fi
+
