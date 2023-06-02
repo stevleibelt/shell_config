4b7b6c4958a7e8e7b779e575ef057567f3363076
stevleibelt <stev.leibelt@hrz.tu-freiberg.de>
2023-06-02 10:03:24 +0200

    Add function net_bazzline_core_ask_yes_or_no

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -110,6 +110,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/).
 
 ### Added
 
+* Added function `net_bazzline_core_ask_yes_or_no` can be used with default values or like `net_bazzline_core_ask_yes_or_no "(n|Y)?" "y"`
 * Added variable `NET_BAZZLINE_IS_ZFS_DKMS` including adaptation of `bin/configure_local_setting.sh` and usage for `software-upgrade-without-kernel`
 * Added function `net_bazzline_shuffle_number` and alias `shuffelNumber`
 * Added [core.sh](_source/functions/core.sh) as place to use for all general purpose functions used below [functions](_source/functions)
--- _source/function/core.sh
+++ _source/function/core.sh
@@ -4,9 +4,46 @@
 #####
 
 ####
+# Asks yes or no
+####
+# [@param1] <string: question_to_ask> default: > Yes or no? Please input n,N,y or Y
+# [@param2] <string: default_answer> default: n
+# returns: 0 if yes, 1 if no
+####
+function net_bazzline_core_ask_yes_or_no ()
+{
+  local QUESTION_TO_ASK
+  local DEFAULT_ANSWER
+
+  QUESTION_TO_ASK="${1:-Yes or no? Please input n,N,y or Y}"
+  DEFAULT_ANSWER="${2:-n}"
+
+  if [[ ${#DEFAULT_ANSWER} -gt 0 ]];
+  then
+    QUESTION_TO_ASK="${QUESTION_TO_ASK} (default is ${DEFAULT_ANSWER})"
+  fi
+  read -r -p "> ${QUESTION_TO_ASK}: "
+
+  if [[ ${REPLY:-${DEFAULT_ANSWER}} =~ ^[yY]$ ]];
+  then
+    return 0
+  elif [[ ${REPLY:-${DEFAULT_ANSWER}} =~ ^[nN]$ ]];
+  then
+    return 1
+  else
+    if [[ ${DEFAULT_ANSWER} =~ ^[yY]$ ]];
+    then
+      return 0
+    else
+      return 1
+    fi
+  fi
+}
+
+####
 # Echos all passed parameters if BE_VERBOSE is greater 0
 ####
-function net_bazzline_core_echo_if_be_verbose()
+function net_bazzline_core_echo_if_be_verbose ()
 {
   if [[ ${BE_VERBOSE} -gt 0 ]];
   then
