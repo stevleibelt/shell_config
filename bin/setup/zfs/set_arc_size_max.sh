70147c03bfa8bd2692e026e68499a93e57ebb36f
stevleibelt <artodeto@bazzline.net>
2023-06-04 23:52:53 +0200

    Change arc cache size to half and add 32 gb

--- bin/setup/zfs/set_arc_size_max.sh
+++ bin/setup/zfs/set_arc_size_max.sh
@@ -9,8 +9,11 @@
 
 function _main ()
 {
-  local CURRENT_SYSTEM_MEMORY=$(grep MemTotal /proc/meminfo | tr -dc '[:digit:]')
-  local FILE_PATH_TO_ZFS_CONF="/etc/modprobe.d/zfs_arc_max.conf"
+  local CURRENT_SYSTEM_MEMORY
+  local FILE_PATH_TO_ZFS_CONF
+
+  CURRENT_SYSTEM_MEMORY=$(grep MemTotal /proc/meminfo | tr -dc '[:digit:]')
+  FILE_PATH_TO_ZFS_CONF="/etc/modprobe.d/zfs_arc_max.conf"
 
   if [[ -f "${FILE_PATH_TO_ZFS_CONF}" ]];
   then
@@ -25,20 +28,24 @@ function _main ()
   #less or equal 4 gb
   if [[ ${CURRENT_SYSTEM_MEMORY} -le 4194304 ]];
   then
-    echo "   Less or exact than 4 GB of memory, setting arc size to 512 MB"
-    sudo bash -c "echo \"options zfs zfs_arc_max=524288\" > ${FILE_PATH_TO_ZFS_CONF}"
+    echo "   Less or exact than 4 GB of memory, setting arc size to 256 MB"
+    sudo bash -c "echo \"options zfs zfs_arc_max=262144\" > ${FILE_PATH_TO_ZFS_CONF}"
   #8 gb
   elif [[ ${CURRENT_SYSTEM_MEMORY} -le 8388608 ]];
   then
-    echo "   Less or exact than 8 GB of memory, setting arc size to 1 GB"
-    sudo bash -c "echo \"options zfs zfs_arc_max=1048576\" > ${FILE_PATH_TO_ZFS_CONF}"
+    echo "   Less or exact than 8 GB of memory, setting arc size to 512 MB"
+    sudo bash -c "echo \"options zfs zfs_arc_max=524288\" > ${FILE_PATH_TO_ZFS_CONF}"
   #16 gb
   elif [[ ${CURRENT_SYSTEM_MEMORY} -le 16777216 ]];
   then
-    echo "   Less or exact than 16 GB of memory, setting arc size to 2 GB"
+    echo "   Less or exact than 16 GB of memory, setting arc size to 1 GB"
+    sudo bash -c "echo \"options zfs zfs_arc_max=1048576\" > ${FILE_PATH_TO_ZFS_CONF}"
+  elif [[ ${CURRENT_SYSTEM_MEMORY} -le 33554432 ]];
+  then
+    echo "   Less or exact than 32 GB of memory, setting arc size to 2 GB"
     sudo bash -c "echo \"options zfs zfs_arc_max=2097152\" > ${FILE_PATH_TO_ZFS_CONF}"
   else
-    echo "   More than 16 GB of memory, setting arc size to 4 GB"
+    echo "   More than 32 GB of memory, setting arc size to 4 GB"
     sudo bash -c "echo \"options zfs zfs_arc_max=4194304\" > ${FILE_PATH_TO_ZFS_CONF}"
   fi
 
