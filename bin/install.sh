475b353f3b41c70ba3527d1fc3f1876c99fdcea0
stevleibelt <stev.leibelt@hrz.tu-freiberg.de>
2023-06-12 11:51:23 +0200

    Add logic to install script to check each step if needed

--- bin/install.sh
+++ bin/install.sh
@@ -12,13 +12,23 @@ function _main ()
 {
   clear
 
+  local ADAPT_BASH_RC
+  local ADAPT_XINIT
+  local PATH_TO_THE_BASH_PROFILE
+  local PATH_TO_THE_BASH_RC
+  local PATH_TO_THE_XINIT
+  local PATH_TO_THIS_SCRIPT
+  local PATH_TO_THE_TEMPORARY_XINIT
+
   #begin of setup
   printf -v DATETIME "%(%F-%T)T" -1
 
+  ADAPT_BASH_RC=1
+  ADAPT_XINIT=1
   PATH_TO_THE_BASH_PROFILE="${HOME}/.bash_profile"
   PATH_TO_THE_BASH_RC="${HOME}/.bashrc"
   PATH_TO_THE_XINIT="${HOME}/.xinitrc"
-  PATH_TO_THIS_SCRIPT=$(cd $(dirname "$0"); pwd)
+  PATH_TO_THIS_SCRIPT=$(cd $(dirname "${0}"); pwd)
 
   PATH_TO_THE_TEMPORARY_XINIT="${PATH_TO_THE_XINIT}.temporary"
   #end of setup
@@ -26,23 +36,49 @@ function _main ()
   ##begin of validation
   if [[ -f "${PATH_TO_THE_BASH_RC}" ]];
   then
-    cat "${PATH_TO_THE_BASH_RC}" | grep -q net_bazzline_config_shell
-
-    if [[ ${?} -eq 0 ]];
+    if grep -q net_bazzline_config_shell "${PATH_TO_THE_BASH_RC}";
     then
-      echo ":: Installation already done."
-      echo "   Will stop here."
-
-      return 0
+      ADAPT_BASH_RC=0
     fi
-  else
-    echo ":: No .bashrc file found."
-    echo "   Bash is currently the only supported shell"
-    
-    echo "   Copying and creating it."
-    cp -v /etc/bash.bashrc ~/.bashrc
+  fi
+
+  if [[ -f "${PATH_TO_THE_XINIT}" ]];
+  then
+    if grep -q net_bazzline_config_shell "${PATH_TO_THE_XINIT}";
+    then
+      ADAPT_XINIT=0
+    fi
   fi
   ##end of validation
+
+  ##begin of adapting bashrc
+  if [[ ${ADAPT_BASH_RC} -gt 0 ]];
+  then
+    echo ":: .bashrc adaptation"
+    if [[ ! -f "${PATH_TO_THE_BASH_RC}" ]];
+    then
+      echo "   No .bashrc file found."
+      echo "   Bash is currently the only supported shell"
+      
+      echo "   Copying and creating it."
+      cp -v /etc/bash.bashrc ~/.bashrc
+    fi
+
+    echo "   Adding content"
+    cat >> "${PATH_TO_THE_BASH_RC}" <<DELIM
+#begin of net_bazzline_config_shell"
+#date: "${DATETIME}
+
+PATH_SHELL_CONFIG="${PATH_TO_THIS_SCRIPT}/.."
+
+if [[ -f "\${PATH_SHELL_CONFIG}/bootstrap" ]];
+then
+    source "\${PATH_SHELL_CONFIG}/bootstrap"
+fi
+#end of net_bazzline_config_shell"
+DELIM
+  fi
+  ##end of adapting bashrc
 
   ##begin of adapting bash_profile
   if [[ ! -f "${PATH_TO_THE_BASH_PROFILE}" ]];
@@ -50,8 +86,8 @@ function _main ()
     echo ":: No .bash_profile file found."
     echo "   Creating on ..."
 
-    touch ${PATH_TO_THE_BASH_PROFILE}
-    cat > ${PATH_TO_THE_BASH_PROFILE} <<DELIM
+    touch "${PATH_TO_THE_BASH_PROFILE}"
+    cat > "${PATH_TO_THE_BASH_PROFILE}" <<DELIM
 #begin of created net_bazzline_config_shell"
 #created at date: "${DATETIME}
 #
@@ -67,30 +103,14 @@ DELIM
   fi
   ##end of adapting bash_profile
 
-  ##begin of adapting bashrc
-  echo ":: Adapting .bashrc"
-
-  cat >> "${PATH_TO_THE_BASH_RC}" <<DELIM
-#begin of net_bazzline_config_shell"
-#date: "${DATETIME}
-
-PATH_SHELL_CONFIG="${PATH_TO_THIS_SCRIPT}/.."
-
-if [[ -f "\${PATH_SHELL_CONFIG}/bootstrap" ]];
-then
-    source "\${PATH_SHELL_CONFIG}/bootstrap"
-fi
-#end of net_bazzline_config_shell"
-DELIM
-  ##end of adapting bashrc
-
   ##begin of creating local user files
   echo ""
-  echo ":: Creating local files"
+  echo ":: Creating local setting files"
 
   declare -a FILES_TO_CREATE=("setting" "variable" "source" "export" "function" "alias" "automatic_start")
 
-  for FILE_TO_CREATE in ${FILES_TO_CREATE[@]}; do
+  for FILE_TO_CREATE in "${FILES_TO_CREATE[@]}";
+  do
     if [[ ! -f ${FILE_TO_CREATE} ]];
     then
       FILE_PATH_NAME="${PATH_TO_THIS_SCRIPT}/../local.${FILE_TO_CREATE}"
@@ -101,14 +121,17 @@ DELIM
   ##end of creating local user files
 
   ##begin of adapting xinitrc
-  echo ""
-  echo ":: Creating .xinitrc.temporary"
+  if [[ ${ADAPT_XINIT} -gt 0 ]];
+  then
+    echo ""
+    echo ":: XINIT adaptation"
+    echo "   Creating .xinitrc.temporary"
 
-  touch "${PATH_TO_THE_TEMPORARY_XINIT}"
+    touch "${PATH_TO_THE_TEMPORARY_XINIT}"
 
-  echo ":: Adding content to ${PATH_TO_THE_TEMPORARY_XINIT}"
+    echo "   Adding content to ${PATH_TO_THE_TEMPORARY_XINIT}"
 
-  cat >> "${PATH_TO_THE_TEMPORARY_XINIT}" <<DELIM
+    cat > "${PATH_TO_THE_TEMPORARY_XINIT}" <<DELIM
 #begin of net_bazzline_config_shell"
 #date: "${DATETIME}
 
@@ -126,22 +149,24 @@ fi
 #end of net_bazzline_config_shell"
 DELIM
 
-  if [[ -f "${PATH_TO_THE_XINIT}" ]];
-  then
-    echo ""
-    echo ":: Copy content of ${PATH_TO_THE_XINIT} into ${PATH_TO_THE_TEMPORARY_XINIT}"
+    if [[ -f "${PATH_TO_THE_XINIT}" ]];
+    then
+      echo "   Copy content of ${PATH_TO_THE_XINIT} into ${PATH_TO_THE_TEMPORARY_XINIT}"
 
-    cat "${PATH_TO_THE_XINIT}" >> "${PATH_TO_THE_TEMPORARY_XINIT}"
+      cat "${PATH_TO_THE_XINIT}" >> "${PATH_TO_THE_TEMPORARY_XINIT}"
+    fi
+
+    echo "   Replacing ${PATH_TO_THE_XINIT} with ${PATH_TO_THE_TEMPORARY_XINIT}"
+
+    mv "${PATH_TO_THE_TEMPORARY_XINIT}" "${PATH_TO_THE_XINIT}"
   fi
-
-  echo ""
-  echo ":: Replacing ${PATH_TO_THE_XINIT} with ${PATH_TO_THE_TEMPORARY_XINIT}"
-
-  mv "${PATH_TO_THE_TEMPORARY_XINIT}" "${PATH_TO_THE_XINIT}"
   ##end of adapting xinitrc
 
   ##begin of creating cache directory
-  mkdir -p ~/.cache/net_bazzline/shell_config
+  if [[ ! -d ~/.cache/net_bazzline/shell_config ]];
+  then
+    mkdir -p ~/.cache/net_bazzline/shell_config
+  fi
   ##end of creating cache directory
 
   ##begin of finish
@@ -150,4 +175,4 @@ DELIM
   ##end of finish
 }
 
-_main ${@}
+_main "${@}"
