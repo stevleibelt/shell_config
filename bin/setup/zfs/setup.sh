b964518cd6410d91159cd167a58c05bcf7c0a53c
stevleibelt <stev.leibelt@hrz.tu-freiberg.de>
2023-06-16 15:48:25 +0200

    Add fix for swap

--- bin/setup/zfs/setup.sh
+++ bin/setup/zfs/setup.sh
@@ -169,27 +169,33 @@ DELIM"
   #bo: swap
   # ref: https://wiki.archlinux.org/title/ZFS#Swap_volume
 
-  if $(swapon -s | wc -l) -eq 0;
+  if ! swapon -s | grep -q '/dev';
   then
     zfs list
-    read -p -r "> Please input local root zpool (e.g. zroot). "
+    read -p "> Please input local root zpool (e.g. zroot). " -r
 
-    sudo zfs create -V 8G             \
-      -b $(getconf PAGESIZE)          \
-      -o compression=zle              \
-      -o logbias=throughput           \
-      -o sync=always                  \
-      -o primarycache=metadata        \
-      -o secondarycache=none          \
-      -o com.sun:auto-snapshot=false  \
-      "${REPLY}/swap"
+    if [[ ${#REPLY} -gt 0 ]];
+    then
 
-    sudo mkswap -f "/dev/zvol/${REPLY}/swap"
-    sudo swapon "/dev/zvol/${REPLY}/swap"
+      sudo zfs create -V 8G             \
+        -b $(getconf PAGESIZE)          \
+        -o compression=zle              \
+        -o logbias=throughput           \
+        -o sync=always                  \
+        -o primarycache=metadata        \
+        -o secondarycache=none          \
+        -o com.sun:auto-snapshot=false  \
+        "${REPLY}/swap"
 
-    sudo bash -c "cat >> /etc/fstab <<DELIM
+      sudo mkswap -f "/dev/zvol/${REPLY}/swap"
+      sudo swapon "/dev/zvol/${REPLY}/swap"
+
+      sudo bash -c "cat >> /etc/fstab <<DELIM
 /dev/zvol/${REPLY}/swap none  swap  discard 0 0
 DELIM"
+    else
+      echo "   Invalid or no pool name provided."
+    fi
   else
     echo "   Skipping swap creation, there is a swap available already"
   fi
