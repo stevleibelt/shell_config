e50a7f1321279ebdf4431afb67ff94134ad87ec3
stevleibelt <artodeto@bazzline.net>
2024-01-09 22:29:53 +0100

    Style net_bazzline_media_rip_cd_as_mp3

--- _source/function/media/audio.sh
+++ _source/function/media/audio.sh
@@ -5,23 +5,22 @@
 ####
 function net_bazzline_media_rip_cd_as_mp3()
 {
-    net_bazzline_record_function_usage ${FUNCNAME[0]}
+  local SELECT_A_RELEASE
+  local SELECTED_RELEASE
 
-    local NAME_OF_THE_SNAPSHOT=$(date +'%Y-%m-%d_%H-%M-%S')
+  if [[ ${#} -gt 0 ]];
+  then
+    SELECT_A_RELEASE=1
+    SELECTED_RELEASE="${1}"
+  else
+    SELECT_A_RELEASE=0
+  fi
 
-    if [[ $# -gt 0 ]];
-    then
-      local SELECT_A_RELEASE=1
-      local SELECTED_RELEASE="${1}"
-    else
-      local SELECT_A_RELEASE=0
-    fi
-
-    if [[ ${SELECT_A_RELEASE} -eq 1 ]];
-    then
-      cyanrip -o mp3 -R ${SELECTED_RELEASE}
-    else
-      cyanrip -o mp3
-    fi
+  if [[ ${SELECT_A_RELEASE} -eq 1 ]];
+  then
+    cyanrip -o mp3 -R "${SELECTED_RELEASE}"
+  else
+    cyanrip -o mp3
+  fi
 }
 
