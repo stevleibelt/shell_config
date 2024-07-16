790f7803d90271ab7a0fa3a22323c9aaf8744d7c
stevleibelt <stev.leibelt@hrz.tu-freiberg.de>
2024-07-16 11:48:07 +0200

    Create net_bazzline_convert_video_to_mkv

--- _source/alias/media.sh
+++ _source/alias/media.sh
@@ -6,6 +6,12 @@ then
   alias addOcrToPdf=net_bazzline_media_book_add_ocr_to_pdf
 fi
 
+#c
+if [[ -f /usr/bin/ffmpeg ]];
+then
+  alias convertVideoToMkv=net_bazzline_convert_video_to_mkv
+fi
+
 #r
 if [[ -f /usr/bin/cyanrip  ]];
 then
