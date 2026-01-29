38fe1f2795eeb1cd0b2d664a5aaac6d263334558
stevleibelt <artodeto@bazzline.net>
2026-01-29 17:57:06 +0100

    Modified skript to a generic solution

--- bin/setup/package_manager/python_rebuild_packages.sh
+++ bin/setup/package_manager/python_rebuild_packages.sh
@@ -7,51 +7,55 @@
 
 function _upgrade()
 {
-    local PACKAGEMANAGER='';
-    local PATH_TO_OUTDATED_PYTHON_PACKAGE='/usr/lib/python3.9'
-    local PATH_TO_LATEST_PYTHON_PACKAGE='/usr/lib/python3.10'
+  local PACKAGEMANAGER
+  local PATH_TO_OUTDATED_PYTHON_PACKAGE
+  local PATH_TO_LATEST_PYTHON_PACKAGE
 
-    #begin of testing if we are on the right system
-    if [[ ! -f /usr/bin/pacman ]];
-    then
-        echo ":: Can not install on your system."
-        echo "   Sorry dude, I can only install things on a arch linux."
+  PACKAGEMANAGER='';
+  PATH_TO_OUTDATED_PYTHON_PACKAGE="/usr/lib/python*"
+  PATH_TO_LATEST_PYTHON_PACKAGE=$(find /usr/lib/ -maxdepth 1 -iname "python*" -type d | sort | uniq | tail -1)
 
-        return 1
-    fi
+  #begin of testing if we are on the right system
+  if [[ ! -f /usr/bin/pacman ]];
+  then
+    echo ":: Can not install on your system."
+    echo "   Sorry dude, I can only install things on a arch linux."
 
-    if [[ -f /usr/bin/yay ]];
-    then
-        PACKAGEMANAGER='yay';
-    elif [[ -f /usr/bin/paru ]];
-    then
-        PACKAGEMANAGER='paru';
-    else
-        echo ":: Can not upgrade your aur python packages."
-        echo "   Neither yay or paru was found."
+    return 1
+  fi
 
-        return 2
-    fi
+  if [[ -f /usr/bin/yay ]];
+  then
+    PACKAGEMANAGER='yay';
+  elif [[ -f /usr/bin/paru ]];
+  then
+    PACKAGEMANAGER='paru';
+  else
+    echo ":: Can not upgrade your aur python packages."
+    echo "   Neither yay or paru was found."
 
-    if [[ ! -d ${PATH_TO_LATEST_PYTHON_PACKAGE} ]];
-    then
-        echo ":: Expected latest python package not found."
-        echo "   >>${PATH_TO_LATEST_PYTHON_PACKAGE}<< is missing."
-        echo "   Please do a systemupdate first."
-        echo "   Just execute something like >>${PACKAGEMANAGER} -Syyu<<."
+    return 2
+  fi
 
-        return 3
-    fi
+  if [[ ! -d ${PATH_TO_LATEST_PYTHON_PACKAGE} ]];
+  then
+    echo ":: Expected latest python package not found."
+    echo "   >>${PATH_TO_LATEST_PYTHON_PACKAGE}<< is missing."
+    echo "   Please do a systemupdate first."
+    echo "   Just execute something like >>${PACKAGEMANAGER} -Syyu<<."
 
-    echo ":: Fetching python packages to upgrade."
-    pacman -Qoq "${PATH_TO_OUTDATED_PYTHON_PACKAGE}"
+    return 3
+  fi
 
-    echo ""
-    echo ":: Rebuilding packages."
-    pacman -Qoq "${PATH_TO_OUTDATED_PYTHON_PACKAGE}" | "${PACKAGEMANAGER}" -S - --rebuild
+  echo ":: Fetching python packages to upgrade."
+  pacman -Qoq "${PATH_TO_OUTDATED_PYTHON_PACKAGE}"
 
-    echo ":: Just for testing ... fetching python packages to upgrade."
-    pacman -Qoq "${PATH_TO_OUTDATED_PYTHON_PACKAGE}"
+  echo ""
+  echo ":: Rebuilding packages."
+  pacman -Qoq "${PATH_TO_OUTDATED_PYTHON_PACKAGE}" | "${PACKAGEMANAGER}" -S - --rebuild
+
+  echo ":: Just for testing ... fetching python packages to upgrade."
+  pacman -Qoq "${PATH_TO_OUTDATED_PYTHON_PACKAGE}"
 }
 
 _upgrade
