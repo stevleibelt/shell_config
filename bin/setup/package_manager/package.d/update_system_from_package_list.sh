f1f033f1573f1c20cec6240677db6d34b183e2cf
stevleibelt <stev.leibelt@hrz.tu-freiberg.de>
2026-08-03 15:59:42 +0200

    Add fix to only remove installed packages and add a demo file content

--- bin/setup/package_manager/package.d/update_system_from_package_list.sh
+++ bin/setup/package_manager/package.d/update_system_from_package_list.sh
@@ -3,16 +3,27 @@
 # @since 2026-08-02
 # @author stev leibelt <artodeto@bazzline.net>
 ####
+# cat > my_package_file.txt <<DELIM
+# ####
+# # The logic is simple
+# # # My comment
+# # +my_ensured_installed_package
+# # -my_ensured_not_installed_package
+# DELIM
+#
+####
 
 function _main()
 {
   #bo: variable
   local FILE_PATH
+  local INSTALLED_PACKAGE_ARRAY_TO_REMOVE
   local PACKAGE_ARRAY_TO_KEEP
   local PACKAGE_ARRAY_TO_REMOVE
   local PACKAGE_MANAGER
 
   FILE_PATH="${1}"
+  INSTALLED_PACKAGE_ARRAY_TO_REMOVE=()
   PACKAGE_ARRAY_TO_KEEP=()
   PACKAGE_ARRAY_TO_REMOVE=()
   PACKAGE_MANAGER="/usr/bin/pacman"
@@ -54,26 +65,39 @@ function _main()
 
   while IFS= read -r LINE;
   do
-    if [[ "${LINE}" == \#* ]];
+    if [[ "${LINE}" == -* ]];
     then
-      # Add to array and remove first #
-      PACKAGE_ARRAY_TO_REMOVE+=("${LINE#\#}")
-    else
-      PACKAGE_ARRAY_TO_KEEP+=("${LINE}")
+      # Add to array and remove first -
+      PACKAGE_ARRAY_TO_REMOVE+=("${LINE#-}")
+    elif [[ "${LINE}" == +* ]];
+    then
+      # Add to array and remove first +
+      PACKAGE_ARRAY_TO_KEEP+=("${LINE#+}")
     fi
   done < "${FILE_PATH}"
 
   if [[ ${#PACKAGE_ARRAY_TO_KEEP[@]} -ne 0 ]];
   then
-    #${PACKAGE_MANAGER} -S "${PACKAGE_ARRAY_TO_KEEP[@]}"
-    printf "${PACKAGE_MANAGER} -S %s\n" "${PACKAGE_ARRAY_TO_KEEP[@]}"
+    echo ":: Ensuring following packages are installed."
+    printf "   %s\n" "${PACKAGE_ARRAY_TO_KEEP[@]}"
+
+    ${PACKAGE_MANAGER} -S --needed "${PACKAGE_ARRAY_TO_KEEP[@]}"
   fi
 
+  for CURRENT_PACKAGE in "${PACKAGE_ARRAY_TO_REMOVE[@]}";
+  do
+    if pacman -Qi "${CURRENT_PACKAGE}" &>/dev/null;
+    then
+      INSTALLED_PACKAGE_ARRAY_TO_REMOVE+=("${CURRENT_PACKAGE}")
+    fi
+  done
 
-  if [[ ${#PACKAGE_ARRAY_TO_REMOVE[@]} -ne 0 ]];
+  if [[ ${#INSTALLED_PACKAGE_ARRAY_TO_REMOVE[@]} -ne 0 ]];
   then
-    #${PACKAGE_MANAGER} -R "${PACKAGE_ARRAY_TO_REMOVE[@]}"
-    printf "${PACKAGE_MANAGER} -R %s\n" "${PACKAGE_ARRAY_TO_REMOVE[@]}"
+    echo ":: Ensuring following packages are removed."
+    printf "   %s\n" "${INSTALLED_PACKAGE_ARRAY_TO_REMOVE[@]}"
+
+    ${PACKAGE_MANAGER} -R "${INSTALLED_PACKAGE_ARRAY_TO_REMOVE[@]}"
   fi
 }
 
